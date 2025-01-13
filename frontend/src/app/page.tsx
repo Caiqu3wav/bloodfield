9ffd71c2f2e02b0dc66a38a33bccd583abdff441
Caique <asapcaique7@gmail.com>
2025-01-13 12:43:51 -0300

    ajustes responsivos na tela de aviso fora de ar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,9 +5,10 @@ export default function Home() {
   
   return (
     <main className="w-full flex items-center justify-center overflow-x-hidden min-h-[700px]"> 
-      <div className="bg-gradient-to-b flex flex-col items-center justify-center from-black to-red-600 w-[40%] h-[400px] rounded-lg">
+      <div className="bg-gradient-to-b flex flex-col items-center justify-center from-black to-red-600 w-[60%] h-[400px] majorfour2:w-[90%] rounded-lg">
         <Image src={Logo} alt="bloodfield logo"/>
-        <h1 className="text-5xl text-center font-bold">Site fora do ar por enquanto!</h1>
+        <h1 className="text-5xl midtwo4:text-2xl text-center font-bold">Site fora do ar por enquanto!</h1>
+        <p className="text-lg midtwo4:text-sm text-center">(Atualização de produtos e ajustes no sistema, Em breve estará disponível)</p>
         <p>Entre em contato: <a href="https://caiques-portfolio.vercel.app" className="text-blue-500 underline">Site do Caique </a></p>
       </div>
     </main>
